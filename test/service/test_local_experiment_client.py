249b59e829fef920ac24390b4c93f0bcc9d10c81
gadial <gadial@gmail.com>
2023-07-19 10:04:06 +0300

    Local client now supports yaml

--- test/service/test_local_experiment_client.py
+++ test/service/test_local_experiment_client.py
@@ -14,6 +14,7 @@
 import unittest
 import uuid
 import json
+import yaml
 from typing import Optional, Dict, Any
 from datetime import datetime, timedelta
 from test.service.ibm_test_case import IBMTestCase
